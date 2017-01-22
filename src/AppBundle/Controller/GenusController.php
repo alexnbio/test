9d2f7424996a0541ccaabd47d904c215fe3d4339
Alex <alexbio2008@yahoo.com>
2017-01-22 18:38:04 +0200

    add some files from tutorial

---
+++ src/AppBundle/Controller/GenusController.php
@@ -0,0 +1,22 @@
+<?php
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class GenusController extends Controller
+{
+    /**
+     * @Route("/genus/{genusName}")
+     */
+    public function showAction($genusName)
+    {
+        $templating = $this->container->get('templating');
+        $html       = $templating->render('genus/show.html.twig', [
+            'name' => $genusName
+        ]);
+        
+        return new Response($html);
+    }
+}
