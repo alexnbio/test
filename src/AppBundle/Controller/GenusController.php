30158ac548285d925749ee30e1d3e4c8c992dcc9
Alex <alexbio2008@yahoo.com>
2017-01-30 19:36:13 +0200

    adding some new modifications to the project related to database fixtured, etc

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -6,28 +6,66 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use AppBundle\Entity\Genus;
 
 class GenusController extends Controller
 {
+        
     /**
-     * @Route("/genus/{genusName}")
+    * @Route("/genus/new")
+    */
+    public function newAction()
+    {    
+        $genus = new Genus();
+        $genus->setName('Octopus'.rand(1, 100));
+        $genus->setSubFamily('Octopodinae');
+        $genus->setSpeciesCount(rand(100, 99999));
+        
+        $em    = $this->getDoctrine()->getManager();
+        $em->persist($genus);
+        $em->flush();
+        
+        return new Response('<html><body>Genus created!</body></html>');
+    }
+    
+        
+    /**
+    * @Route("/genus")
+    */
+    public function lsitAction()
+    {    
+        $em = $this->getDoctrine()->getManager();
+        
+        $genuses = $em->getRepository('AppBundle:Genus')
+            ->findAll();
+        
+        return $this->render('genus/list.html.twig', array(
+            'genuses' => $genuses,
+        ));
+    }
+    
+    /**
+     * @Route("/genus/{genusName}", name="genus_show")
      */
     public function showAction($genusName)
     {
-        $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
-//         $funFact = $this->get('markdown.parser')->transform($funFact);
-        $cache   = $this->get('doctrine_cache.providers.my_markdown_cache');
-        $key     = md5($funFact);
-        if ($cache->contains($key)) {
-            $funFact = $cache->fetch($key);
-        } else {
-            sleep(1); //fake how slow this could be
-            $funFact = $this->get('markdown.parser')->transform($funFact);
-            $cache->save($key, $funFact);
+        $em    = $this->getDoctrine()->getManager();
+        $genus = $em->getRepository('AppBundle:Genus')
+            ->findOneBy(['name' => $genusName]);
+//         $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
+//         $key   = md5($funFact);
+//         IF ($CACHE->CONTAINS($KEY)) {
+//             $FUNFACT = $CACHE->FETCH($KEY);
+//         } ELSE {
+//             SLEEP(1); //FAKE HOW SLOW THIS COULD BE
+//             $FUNFACT = $THIS->GET('MARKDOWN.PARSER')->TRANSFORM($FUNFACT);
+//             $CACHE->SAVE($KEY, $FUNFACT);
+//         }
+        if (!$genus) {
+            throw $this->createNotFoundException('genus not found');
         }
         return $this->render('genus/show.html.twig', [
-            'name'    => $genusName,
-            'funFact' => $funFact,
+            'genus' => $genus,
         ]);
     }
     
--- src/AppBundle/Entity/Genus.php
+++ src/AppBundle/Entity/Genus.php
@@ -33,4 +33,80 @@ class Genus {
      * @ORM\Column(type="string")
      */
     private $name;
+    
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $subFamily;
+    
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $speciesCount;
+    
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $funFact;
+    
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isPublished = true;
+    
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getSubFamily()
+    {
+        return $this->subFamily;
+    }
+
+    public function setSubFamily($subFamily)
+    {
+        $this->subFamily = $subFamily;
+        return $this;
+    }
+
+    public function getSpeciesCount()
+    {
+        return $this->speciesCount;
+    }
+
+    public function setSpeciesCount($speciesCount)
+    {
+        $this->speciesCount = $speciesCount;
+        return $this;
+    }
+
+    public function getFunFact()
+    {
+        return $this->funFact;
+    }
+
+    public function setFunFact($funFact)
+    {
+        $this->funFact = $funFact;
+        return $this;
+    }
+    
+    public function getUpdatedAt()
+    {
+        return new \DateTime('-'.rand(0, 100).' days');
+    }
+
+    public function setIsPublished($isPublished)
+    {
+        $this->isPublished = $isPublished;
+        return $this;
+    }
+    
 }
